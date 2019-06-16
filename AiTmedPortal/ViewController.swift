90c7ff04230121bbc8cbc66e55a4ca691720d237
yi tong <tyacal9527@gmail.com>
2019-06-16 15:53:47 -0700

    Initial Commit

---
+++ AiTmedPortal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AiTmedPortal
+//
+//  Created by tongyi on 6/16/19.
+//  Copyright © 2019 tongyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
